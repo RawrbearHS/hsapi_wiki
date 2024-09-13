103df500b6d0e6450af1341a4f63c61b87b313f4
RawrbearHS <75764728+RawrbearHS@users.noreply.github.com>
2024-09-12 19:57:33 -0500

    Add all properties to wiki

--- src/components/Undocumented.tsx
+++ src/components/Undocumented.tsx
@@ -3,6 +3,6 @@ type Undocumented = {
 }
 
 export default function Undocumented({message}: Undocumented) {
-          if (message) return (<p>{message}</p>);
+          if (message) return (<p><i>{message}</i></p>);
           else return (null);
 }
